8d83ea03f3d680b74059caf2b0bf05f352297c04
priyanshush325 <priyanshush325@gmail.com>
2023-06-16 12:15:43 -0700

    added functionality + styling for RottenTomatoes reviews

--- src/components/RottenTomatoesTile.js
+++ src/components/RottenTomatoesTile.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import './RottenTomatoes.scss';
 
 export const RottenTomatoesTile = (props) => {
@@ -10,21 +10,55 @@ export const RottenTomatoesTile = (props) => {
 
     let userState;
     let criticState;
+    let activeReviews = ["test1", "test2", "test3", "test4"];
+
+    useEffect(() => {
+        fetch("https://movie-finder-backend.onrender.com/rottenTomatoes/user/" + movie)
+        .then((res) => res.json())
+        .then((json) => {
+            console.log("got user reviews", json);
+            setUserReviews(json["reviews"]);
+        });
+        fetch("https://movie-finder-backend.onrender.com/rottenTomatoes/critic/" + movie)
+        .then((res) => res.json())
+        .then((json => {
+            console.log("got critic reviews", json);
+            setCriticReviews(json["reviews"]);
+        }))
+    }, [movie]);
 
     if (activeButton === "critic"){
         userState = "";
         criticState = "selected";
+        activeReviews = criticReviews;
     }
     else{
         userState = "selected";
         criticState = "";
+        activeReviews = userReviews;
+    }
+
+    const handleClick = (buttonType) => {
+        if (buttonType === 1){
+            setActiveButton("critic");
+            activeReviews = criticReviews;
+        }
+        if (buttonType === 2){
+            setActiveButton("user");
+            activeReviews = userReviews;
+        }
     }
 
     return (
         <div className = "rottenTomatoesTile">
             <div className = "buttonContainer">
-                <button className = {`criticButton ${criticState}`} onClick = {() => {setActiveButton("critic")}}>Critic Reviews</button>
-                <button className = {`userButton ${userState}`} onClick = {() => {setActiveButton("user")}}>User Reviews</button>
+                <button className = {`criticButton ${criticState}`} onClick = {() => {handleClick(1)}}>Critic Reviews</button>
+                <button className = {`userButton ${userState}`} onClick = {() => {handleClick(2)}}>User Reviews</button>
+            </div>
+            <div className = "reviewContainer">
+                    {activeReviews.map((review, index) => {
+                        return <div className = "reviewBox">{review}</div>
+                    })}
             </div>
         </div>
     )
